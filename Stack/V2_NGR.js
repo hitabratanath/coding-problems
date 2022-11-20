03adcd51782202412217ea3ee4f4187451eca6f2
hitabratanath <34376577+hitabratanath@users.noreply.github.com>
2022-11-20 08:49:12 +0530

    NGR & NGL without cyclic

---
+++ Stack/V2_NGR.js
@@ -0,0 +1,28 @@
+function NGR(arr) {
+  let stk = [],
+    result = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      result.push(-1);
+    } else if (stk[stk.length - 1] > arr[i]) {
+      result.push(stk[stk.length - 1]);
+      stk.push(arr[i]);
+    } else {
+      while (stk.length !== 0 && stk[stk.length - 1] < arr[i]) {
+        stk.pop();
+      }
+      if (stk.length === 0) {
+        result.push(-1);
+        stk.push(arr[i]);
+      } else {
+        result.push(stk[stk.length - 1]);
+        stk.push(arr[i]);
+      }
+    }
+  }
+  return result.reverse();
+}
+const arr = [1, 3, 2, 4];
+console.log(NGR(arr));
